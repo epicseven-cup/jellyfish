95e228291c18ac61f70b0512b55d33d21f75a09e
epicseven-cup <jackylin616@gmail.com>
2025-02-20 17:59:22 -0500

    update

--- jellyfish-frontend/src/components/HeaderMegaMenu/HeaderMegaMenu.tsx
+++ jellyfish-frontend/src/components/HeaderMegaMenu/HeaderMegaMenu.tsx
@@ -4,87 +4,40 @@
 
 
 import {
-    IconBook,
-    IconChartPie3,
-    IconChevronDown,
-    IconCode,
-    IconCoin,
-    IconFingerprint,
-    IconNotification,
-    IconBrandGitlab
-  } from '@tabler/icons-react';
-  import {
-    Anchor,
-    Box,
-    Burger,
-    Button,
-    Center,
-    Collapse,
-    Divider,
-    Drawer,
-    Group,
-    HoverCard,
-    ScrollArea,
-    SimpleGrid,
-    Text,
-    ThemeIcon,
-    UnstyledButton,
-    useMantineTheme,
-  } from '@mantine/core';
-  import { useDisclosure } from '@mantine/hooks';
-//   import { MantineLogo } from '@mantinex/mantine-logo';
-  import classes from './HeaderMegaMenu.module.css';
-  
-  const mockdata = [
-    {
-      icon: IconCode,
-      title: 'Open source',
-      description: 'This Pokémon’s cry is very loud and distracting',
-    },
-    {
-      icon: IconCoin,
-      title: 'Free for everyone',
-      description: 'The fluid of Smeargle’s tail secretions changes',
-    },
-    {
-      icon: IconBook,
-      title: 'Documentation',
-      description: 'Yanma is capable of seeing 360 degrees without',
-    },
-    {
-      icon: IconFingerprint,
-      title: 'Security',
-      description: 'The shell’s rounded shape and the grooves on its.',
-    },
-    {
-      icon: IconChartPie3,
-      title: 'Analytics',
-      description: 'This Pokémon uses its flying ability to quickly chase',
-    },
-    {
-      icon: IconNotification,
-      title: 'Notifications',
-      description: 'Combusken battles with the intensely hot flames it spews',
-    },
-  ];
-  
-  export function HeaderMegaMenu() {
-    const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-    const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-    const theme = useMantineTheme();
-  
-  
-    return (
-      <Box pb={120}>
-        <header className={classes.header}>
-          <Group  justify="space-between" h="100%">
-            <IconBrandGitlab size={30} />
-            <Group visibleFrom="sm">
-              <Button variant="default">Log in</Button>
-              <Button>Sign up</Button>
-            </Group>
+  IconBrandGitlab
+} from '@tabler/icons-react';
+import {
+  Box,
+  Button,
+  Group,
+} from '@mantine/core';
+import classes from './HeaderMegaMenu.module.css';
+
+
+export function HeaderMegaMenu() {
+
+
+  return (
+    <Box pb={120}>
+      <header className={classes.header}>
+        <Group justify="space-between" h="100%">
+          <IconBrandGitlab size={30} />
+
+          <Group h="100%" gap={0} visibleFrom="sm" ta='center'>
+            <a href="#" className={classes.link}>Home</a>
+            <a href="#" className={classes.link}>Home</a>
+            <a href="#" className={classes.link}>Home</a>
+            <a href="#" className={classes.link}>Home</a>
+            <a href="#" className={classes.link}>Home</a>
           </Group>
-        </header>
-      </Box>
-    );
-  }
+
+
+          <Group visibleFrom="sm">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+        </Group>
+      </header>
+    </Box>
+  );
+}
