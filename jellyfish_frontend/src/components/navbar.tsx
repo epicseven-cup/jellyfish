df604c297dfd4d754dcf4ec95cd39959183c5a83
epicseven-cup <jackylin616@gmail.com>
2025-02-20 20:49:57 -0500

    push frontend

---
+++ jellyfish_frontend/src/components/navbar.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@heroui/button";
+import { Kbd } from "@heroui/kbd";
+import { Link } from "@heroui/link";
+import { Input } from "@heroui/input";
+import {
+  Navbar as HeroUINavbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@heroui/navbar";
+
+import { siteConfig } from "@/config/site";
+import { ThemeSwitch } from "@/components/theme-switch";
+import {
+  GithubIcon,
+} from "@/components/icons";
+import clsx from "clsx";
+import { link as linkStyles } from "@heroui/theme";
+
+export const Navbar = () => {
+
+
+  return (
+    <HeroUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand className="gap-3 max-w-fit">
+          <Link
+            className="flex justify-start items-center gap-1"
+            color="foreground"
+            href="/"
+          >
+            <p className="font-bold text-inherit">Jellyfish</p>
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {siteConfig.navItems.map((item) => (
+          <NavbarItem>
+            <Link className={clsx(
+              linkStyles({ color: "foreground" }),
+              "data-[active=true]:text-primary data-[active=true]:font-medium"
+            )}
+            color="foreground"
+            href={item.href}
+            >{item.label}</Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+          <Button size="sm">Login</Button>
+          <Button size="sm">Signup</Button>
+          <Link isExternal href={siteConfig.links.github} title="GitHub">
+            <GithubIcon className="text-default-500" />
+          </Link>
+          <ThemeSwitch />
+        </NavbarItem>
+      </NavbarContent>
+
+    </HeroUINavbar>
+  );
+};
---
+++ jellyfish_frontend/src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from "@heroui/link";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.github}
+          >
+            <GithubIcon size={20} />
+            GitHub
+          </Link>
+        </div>
+
+      </section>
+    </DefaultLayout>
+  );
+}
